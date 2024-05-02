cd31d5dc0fb7526c87ea4ced5530cc6ffc9a2ca2
huybang2017 <ndhbang2017@gmail.com>
2024-05-02 11:02:58 +0700

    feat: disable border line animate

---
+++ lua/plugins/style.lua
@@ -0,0 +1,16 @@
+return {
+  "mini.indentscope",
+  opts = {
+    draw = {
+      delay = 0,
+      animation = function()
+        return 0
+      end,
+    },
+    options = {
+      border = "bottom",
+      indent_at_cursor = false,
+      try_as_border = false,
+    },
+  },
+}
