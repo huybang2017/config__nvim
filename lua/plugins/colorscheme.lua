b228f0101b4e416685968d8da1f21c62d3b5765e
huybang2017 <ndhbang2017@gmail.com>
2024-05-02 11:02:37 +0700

    feat: config color theme and load

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,35 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
+    priority = 100,
     opts = { style = "night" },
+  },
+
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
+  { -- monet
+    "fynnfluegge/monet.nvim",
+    lazy = true,
+    priority = 100,
+    event = "VeryLazy",
+  },
+
+  { -- visual_studio_code
+    "askfiy/visual_studio_code",
+    lazy = true,
+    priority = 100,
+    event = "VeryLazy",
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "visual_studio_code",
+    },
   },
 }
